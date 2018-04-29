b54d3f9ed690841c60cf500772e94cb7edfb68b3
lepiklark <AleksandarTomic88@gmail.com>
2018-04-29 17:08:31 +0200

    Introducing InitializeTimeHooks and removing static declarations for every hooked functions.

--- ChronomorphLib/DllMain.cpp
+++ ChronomorphLib/DllMain.cpp
@@ -1,23 +1,26 @@
+#include "stdafx.h"
 #include <Windows.h>
+
+void InitializeTimeHooks();
 
 bool WINAPI DllMain(HINSTANCE hInstDll, DWORD fdwReason, PVOID fImpLoad) 
 {
 	OutputDebugString(L"Entering Dll main.");
 
-	switch (fdwReason) {
+	switch (fdwReason) 
+	{
 
 	case DLL_THREAD_ATTACH:
 		break;
 
 	case DLL_PROCESS_ATTACH:
+		InitializeTimeHooks();
 		break;
 
 	case DLL_THREAD_DETACH:
 
 	case DLL_PROCESS_DETACH:
-		// TODO: Do all the cleanup here.
-		//
 		break;
 	}
-	return(TRUE);
+	return(true);
 }
--- ChronomorphLib/TimeHooks.cpp
+++ ChronomorphLib/TimeHooks.cpp
@@ -3,34 +3,47 @@
 #include "APIHook.h"
 #include "ChronoModeEnum.h"
 
-CAPIHook CAPIHook::sm_GetLocalTime("Kernel32.dll", "GetLocalTime", (PROC)CAPIHook::GetLocalTime);
+#include <vector>
 
-void WINAPI CAPIHook::GetLocalTime(_Out_ LPSYSTEMTIME lpSystemTime)
+struct InputArgs
+{
+	int offset = 0;
+};
+
+std::vector<CAPIHook*> g_timeHooks;
+InputArgs g_InputArgs;
+
+void WINAPI GetLocalTime_Hook(_Out_ LPSYSTEMTIME lpSystemTime)
 {
 	::GetLocalTime(lpSystemTime);
 
+	int offset = g_InputArgs.offset;
+	lpSystemTime->wHour = lpSystemTime->wHour + offset;
+}
+
+void WINAPI GetSystemTime_Hook(_Out_ LPSYSTEMTIME lpSystemTime)
+{
+	::GetSystemTime(lpSystemTime);
+}
+
+void InitializeTimeHooks()
+{
 	wchar_t envMode[MAX_PATH];
 	GetEnvironmentVariable(CHRONO_MODE_ARG, envMode, MAX_PATH);
+	ChronoModeEnum chronoMode = static_cast<ChronoModeEnum>(_wtoi(envMode));
 
 	wchar_t envArg[MAX_PATH];
 	GetEnvironmentVariable(CHRONO_MODE_ARG_1, envArg, MAX_PATH);
 
-	int offset = _wtoi(envArg);
+	if (chronoMode == ChronoModeEnum::TimeOffset)
+	{
+		int offset = _wtoi(envArg);
+		g_InputArgs.offset = offset;
 
-	// TODO: Hard coding for initial testing.
-	// This should be passed as an argument from time morpher.
-	//
-	lpSystemTime->wHour = lpSystemTime->wHour + offset;
-}
+		CAPIHook* getLocalTimeHook = new CAPIHook("Kernel32.dll", "GetLocalTime", (PROC)GetLocalTime_Hook);
+		CAPIHook* getSystemTimeHook = new CAPIHook("Kernel32.dll", "GetSystemTime", (PROC)GetSystemTime_Hook);
 
-CAPIHook CAPIHook::sm_GetSystemTime("Kernel32.dll", "GetSytemTime", (PROC)CAPIHook::GetSystemTime);
-
-void WINAPI CAPIHook::GetSystemTime(_Out_ LPSYSTEMTIME lpSystemTime)
-{
-	::GetSystemTime(lpSystemTime);
-
-	// TODO: Hard coding for initial testing.
-	// This should be passed as an argument from time morpher.
-	//
-	// lpSystemTime->wHour = lpSystemTime->wHour - 5;
+		g_timeHooks.push_back(getLocalTimeHook);
+		g_timeHooks.push_back(getSystemTimeHook);
+	}
 }
